41c4fe04c481046e06e90f511563556322bc6826
ratul2f8 <ratuldasanik@outlook.com>
2023-08-11 02:36:17 +0600

    v1

--- image-compressor-api/Controllers/WeatherForecastController.cs
+++ image-compressor-api/Controllers/WeatherForecastController.cs
@@ -1,4 +1,7 @@
+using System.Security.Cryptography;
+using System.Text;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace image_compressor_api.Controllers;
 
@@ -8,7 +11,16 @@ public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
     {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "Freezing",
+        "Bracing",
+        "Chilly",
+        "Cool",
+        "Mild",
+        "Warm",
+        "Balmy",
+        "Hot",
+        "Sweltering",
+        "Scorching"
     };
 
     private readonly ILogger<WeatherForecastController> _logger;
@@ -21,12 +33,72 @@ public class WeatherForecastController : ControllerBase
     [HttpGet(Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get()
     {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        return Enumerable
+            .Range(1, 5)
+            .Select(
+                index =>
+                    new WeatherForecast
+                    {
+                        Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                        TemperatureC = Random.Shared.Next(-20, 55),
+                        Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                    }
+            )
+            .ToArray();
+    }
+
+    [HttpGet("fire-and-forget")]
+    public async Task<string> fireAndForgetController()
+    {
+        var httpTestService = new HttpTestService();
+        await httpTestService.fireAndForget(
+            "http://localhost:8080/v1/media-trace/update-status",
+            new
+            {
+                key = "test",
+                status = "SUCCESS",
+                objectURL = "https://www.google.com"
+            },
+            "ff8dd99b-abe0-44c6-89c5-d7b4d47924a4"
+        );
+        return "Done";
+    }
+}
+
+public class HttpTestService
+{
+    public async Task fireAndForget(string uri, object payload, string? signature)
+    {
+        try
         {
-            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
+            Uri parsedUri;
+            if (
+                Uri.TryCreate(uri, UriKind.Absolute, out parsedUri)
+                && (parsedUri.Scheme == Uri.UriSchemeHttp || parsedUri.Scheme == Uri.UriSchemeHttps)
+                && signature?.Length > 1
+            )
+            {
+                string payloadJSON = JsonConvert.SerializeObject(payload);
+                byte[] secretBytes = Encoding.UTF8.GetBytes(signature);
+                using HMACSHA256 hmac = new HMACSHA256(secretBytes);
+                byte[] hashBytes = hmac.ComputeHash(Encoding.UTF8.GetBytes(payloadJSON));
+                string xsignature =
+                    "sha256=" + BitConverter.ToString(hashBytes).Replace("-", "").ToLower();
+
+                StringContent content = new StringContent(
+                    payloadJSON,
+                    Encoding.UTF8,
+                    "application/json"
+                );
+                using var httpClient = new HttpClient { Timeout = TimeSpan.FromSeconds(2) };
+                httpClient.DefaultRequestHeaders.Add("X-Signature", xsignature);
+                var response = await httpClient.PostAsync(uri, content);
+                return;
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
     }
 }
--- lambda-image-processor/src/lambda-image-processor/Function.cs
+++ lambda-image-processor/src/lambda-image-processor/Function.cs
@@ -1,8 +1,11 @@
+using System.Security.Cryptography;
+using System.Text;
 using Amazon.Lambda.Core;
 using Amazon.Lambda.S3Events;
 using Amazon.S3;
 using Amazon.S3.Model;
 using image_processor_classlib.utils;
+using Newtonsoft.Json;
 
 [assembly: LambdaSerializer(
     typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer)
@@ -12,7 +15,8 @@ namespace lambda_image_processor;
 
 public class Function
 {
-    private static readonly string prefix = "assets";
+    private static readonly string _prefix = "assets";
+    private static readonly string _bucketLocationName = "us-east-2";
     IAmazonS3 S3Client { get; set; }
 
     public Function()
@@ -30,6 +34,11 @@ public class Function
         var eventRecords = evnt.Records ?? new List<S3Event.S3EventNotificationRecord>();
         foreach (var record in eventRecords)
         {
+            string signatureSecret = Environment.GetEnvironmentVariable("SIGNATURE_SECRET");
+            string postProcessorURI = "";
+            string mediaKey = "";
+            string objectURL = "";
+
             var s3Event = record.S3;
             if (s3Event == null)
             {
@@ -42,8 +51,8 @@ public class Function
                 var objectKey = s3Event.Object.Key;
                 var response = await S3Client.GetObjectMetadataAsync(bucketName, objectKey);
                 var contentType = response.Headers.ContentType;
-                // context.Logger.LogDebug($"Key is: {objectKey}. Content Type is: {contentType}");
-                Console.WriteLine($"Key is: {objectKey}. Content Type is: {contentType}");
+                // context.Logger.LogInformation($"Key is: {objectKey}. Content Type is: {contentType}");
+                // Console.WriteLine($"Key is: {objectKey}. Content Type is: {contentType}");
                 if (
                     !response.Headers.ContentType.Contains("image/")
                     || response.Metadata["x-amz-meta-processed"] == true.ToString()
@@ -58,8 +67,17 @@ public class Function
                 );
                 using var outputStream = new MemoryStream();
                 await ImageProcessorUtils.ConvertImageToWebP(inputStream, outputStream);
-                var key =
-                    response.Metadata["x-amz-preferred-key"] ?? $"{prefix}/{Guid.NewGuid()}.webp";
+                var guid = Guid.NewGuid().ToString();
+                mediaKey = response.Metadata["x-amz-meta-external-id"] ?? "";
+                postProcessorURI = response.Metadata["x-amz-meta-post-processing-url"] ?? "";
+                var key = response.Metadata["x-amz-meta-preferred-key"] ?? $"{_prefix}/{guid}.webp";
+                // var bucketLocationResponse = await S3Client.GetBucketLocationAsync(
+                //     new GetBucketLocationRequest { BucketName = bucketName }
+                // );
+                // objectURL =
+                //     $"https://{bucketName}.s3.{bucketLocationResponse.Location.Value}.amazonaws.com/{key}";
+
+                objectURL = $"https://{bucketName}.s3.{_bucketLocationName}.amazonaws.com/{key}";
                 await S3Client.PutObjectAsync(
                     new PutObjectRequest
                     {
@@ -70,6 +88,17 @@ public class Function
                         InputStream = outputStream
                     }
                 );
+
+                await this.FireAndForget(
+                    postProcessorURI,
+                    new
+                    {
+                        key = mediaKey,
+                        status = "SUCCESS",
+                        objectURL
+                    },
+                    signatureSecret
+                );
             }
             catch (Exception e)
             {
@@ -78,8 +107,53 @@ public class Function
                 // );
                 context.Logger.LogError(e.Message);
                 // context.Logger.LogError(e.StackTrace);
+                await this.FireAndForget(
+                    postProcessorURI,
+                    new
+                    {
+                        key = mediaKey,
+                        status = "FAILED",
+                        objectURL
+                    },
+                    signatureSecret
+                );
                 throw;
             }
+        }
+    }
+
+    private async Task FireAndForget(string uri, object payload, string? signature)
+    {
+        try
+        {
+            Uri parsedUri;
+            if (
+                Uri.TryCreate(uri, UriKind.Absolute, out parsedUri)
+                && (parsedUri.Scheme == Uri.UriSchemeHttp || parsedUri.Scheme == Uri.UriSchemeHttps)
+                && signature?.Length > 1
+            )
+            {
+                string payloadJSON = JsonConvert.SerializeObject(payload);
+                byte[] secretBytes = Encoding.UTF8.GetBytes(signature);
+                using HMACSHA256 hmac = new HMACSHA256(secretBytes);
+                byte[] hashBytes = hmac.ComputeHash(Encoding.UTF8.GetBytes(payloadJSON));
+                string xsignature =
+                    "sha256=" + BitConverter.ToString(hashBytes).Replace("-", "").ToLower();
+
+                StringContent content = new StringContent(
+                    payloadJSON,
+                    Encoding.UTF8,
+                    "application/json"
+                );
+                using var httpClient = new HttpClient { Timeout = TimeSpan.FromSeconds(2) };
+                httpClient.DefaultRequestHeaders.Add("X-Signature", xsignature);
+                var response = await httpClient.PostAsync(uri, content);
+                return;
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
         }
     }
 }
